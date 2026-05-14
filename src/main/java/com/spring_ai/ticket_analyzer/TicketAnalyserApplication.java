4b7372ae8168b8cc9725a65cfd6e65ef2f3c3562
Aniket455 <aniketsgupta0302@gmail.com>
2026-05-14 09:16:14 +0530

    Initial commit - Spring AI ticket analyzer

---
+++ src/main/java/com/spring_ai/ticket_analyzer/TicketAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.spring_ai.ticket_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAnalyserApplication.class, args);
+	}
+
+}
